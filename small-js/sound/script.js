2ab41485ea580392766e19f738eb437ef6316b30
sherryswin9 <iku4722@hotmail.com>
2021-07-03 13:49:37 +0800

    sound-board

---
+++ small-js/sound/script.js
@@ -0,0 +1,29 @@
+'strict';
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+sounds.forEach(sound =>{
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
+
+    btn.innerText = sound;
+    document.getElementById('buttons').appendChild(btn);
+
+    btn.addEventListener('click', () => {
+
+        stopSongs();
+
+        document.getElementById(sound).play();
+        
+    })
+
+
+
+});
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song =  document.getElementById(sound);
+        song.pause();
+        song.currentTime = 0;
+    })
+}

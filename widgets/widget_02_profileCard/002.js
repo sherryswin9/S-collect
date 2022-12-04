eb9d6592b21034240b513c755e9328e909582575
sherryswin9 <60725727+sherryswin9@users.noreply.github.com>
2022-12-04 20:10:01 +0800

    Add files via upload

---
+++ widgets/widget_02_profileCard/002.js
@@ -0,0 +1,24 @@
+const clc = document.querySelector(".cancel");
+const arr = document.querySelector(".arr_container");
+const left_container = document.querySelector(".left_container"); 
+
+
+clc.addEventListener("click", ()=>{
+   clc.classList.remove("active_arr");
+   if(left_container.classList.contains("active")){
+    left_container.classList.remove("active");
+    left_container.classList.add("off");
+   }
+
+});
+
+arr.addEventListener("click", ()=>{
+   arr.classList.remove("active_arr");
+   if(left_container.classList.contains("off")){
+    left_container.classList.remove("off");
+    left_container.classList.add("active");
+   }
+
+});
+
+
